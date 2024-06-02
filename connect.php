72074dfa0f9b5633bb63ce610f9e1347210d7272
Yokendrakumar P <147273334+Yokendrakumar1999@users.noreply.github.com>
2024-06-02 16:05:42 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$host="localhost";
-$user="root";
-$pass="";
+$host="login.clk6qshkuq1n.us-east-1.rds.amazonaws.com";
+$user="admin";
+$pass="password";
 $db="login";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
