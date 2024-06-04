3e987134ab1e4b296520c4c4d32e48350077de9d
Yokendrakumar P <147273334+Yokendrakumar1999@users.noreply.github.com>
2024-06-04 10:38:56 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -10,7 +10,7 @@ include("connect.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+    <title>home</title>
 </head>
 <body>
     <div style="text-align:center; padding:15%;">
